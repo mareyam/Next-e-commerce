b10dd3cd6c3d59669c06fcb1124e5de1ffb30b5a
maryam <maryaam2209@gmail.com>
2023-08-25 22:50:36 +0500

    redesigning hero page 2

--- components/products/components/ProductItems.js
+++ components/products/components/ProductItems.js
@@ -18,14 +18,20 @@ import { useProducts } from "../../../hooks/useProducts";
 import { useDispatch } from "react-redux";
 import { addCartProduct } from "../../../toolkit/cartSlice";
 
+
 const ProductsItems = ({ itemSplice }) => {
   const { data } = useProducts();
   const dispatch = useDispatch();
+  const [showOverlay, setShowOverlay] = useState(false);
 
   const handleAddToCart = (product) => {
     console.log("Selected Product:", product.title);
     dispatch(addCartProduct(product));
+  };  
+  const toggleOverlay = () => {
+    setShowOverlay(!showOverlay);
   };
+
 
   return (
     <Container maxW="container.lg" border="2px solid">
@@ -33,11 +39,12 @@ const ProductsItems = ({ itemSplice }) => {
         <Heading as="h4">Products</Heading>
         <Text fontSize="12px">Order it for you or for your beloved ones</Text>
       </Box>
-      <SimpleGrid columns={{ base: 2, md: 4 }} spacing="2">
+      <SimpleGrid columns={{ base: 1, md: 4 }} spacing="2">
         {data?.slice(0, itemSplice).map((item) => (
           <Box
             key={item.id}
-            boxSize="250px"
+            // boxSize="250px"
+            width="100%"
             borderWidth="1px"
             borderRadius="lg"
             overflow="hidden"
@@ -45,11 +52,41 @@ const ProductsItems = ({ itemSplice }) => {
             backgroundSize="cover"
             backgroundPosition="center"
             bgImage={item.image}
+            position="relative"
+            onMouseEnter={toggleOverlay}
+            onMouseLeave={toggleOverlay}
           >
-            <Box border='2px solid' marginTop='60%'  flexDirection= 'column'  justifyContent= 'flex-end'>
-              <Stack mt="2" spacing="3" p="2">
-                <Flex justifyContent='space-between'><Text color="green.600" fontSize="sm">
-                  ${item.price}
+            <Box border='2px solid' marginTop='50%'  flexDirection= 'column'  justifyContent= 'flex-end'>
+            {showOverlay && (
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bg="rgba(0, 0, 0, 0.6)"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button colorScheme="green" size="sm" onClick={() => console.log("item")}>
+            Add to Cart
+          </Button>
+          <Button colorScheme="green" size="sm" onClick={() => setButton(!button)}>
+            X
+          </Button>
+          
+        </Box>
+      )}
+
+      
+              <Stack mt="2" spacing="5" p="2">
+               <Heading overflow="hidden" size="sm" isTruncated>
+                  {item.title}
+                </Heading>
+                <Flex justifyContent='space-between' alignItems='center'>
+                <Text fontSize="sm">
+                  Price: ${item.price}
                 </Text>
                 <IconButton
                   w='auto'
@@ -58,9 +95,7 @@ const ProductsItems = ({ itemSplice }) => {
                   backgroundColor="gray.300"
                   icon={<Box>&#128722;</Box>}
                 ></IconButton></Flex>
-                <Heading overflow="hidden" size="xs" isTruncated>
-                  {item.title}
-                </Heading>
+                
               </Stack>
             </Box>
           </Box>
--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,67 @@
-import React, { useState } from "react";
-import ProductsTest from "../components/test/ProductsTest"; 
+import React, { useState } from 'react';
+import { Box, Button, Image, Text } from '@chakra-ui/react';
 
-const Test = () => {
-  const [showAddToCart, setIsShowAddToCart] = useState(true);
+const Test = ({ item }) => {
+  const [button, setButton] = useState(false);
+
+
+  const toggleOverlay = () => {
+    setShowOverlay(!showOverlay);
+  };
+
   return (
-    <div>
-      <ProductsTest showAddToCart={showAddToCart} />
-    </div>
+    <Box
+      position="relative"
+      onMouseEnter={toggleOverlay}
+      onMouseLeave={toggleOverlay}
+    >
+      {/* <Image src={item.imageUrl} alt={item.title} /> */}
+
+
+      {showOverlay && (
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bg="rgba(0, 0, 0, 0.6)"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button colorScheme="green" size="sm" onClick={() => console.log("item")}>
+            Add to Cart
+          </Button>
+          <Button colorScheme="green" size="sm" onClick={() => setButton(!button)}>
+            X
+          </Button>
+          
+        </Box>
+      )}
+
+      <Box p="2">
+        <Text fontSize="lg" fontWeight="bold">
+          maryam 
+        </Text>
+        <Text color="gray.500">$50</Text>
+      </Box>
+    </Box>
   );
 };
 
 export default Test;
+
+// import React, { useState } from "react";
+// import ProductsTest from "../components/test/ProductsTest"; 
+
+// const Test = () => {
+//   const [showAddToCart, setIsShowAddToCart] = useState(true);
+//   return (
+//     <div>
+//       <ProductsTest showAddToCart={showAddToCart} />
+//     </div>
+//   );
+// };
+
+// export default Test;
