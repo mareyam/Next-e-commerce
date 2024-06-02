fadf2b2f2eb703ec439ec024caf6592684075210
Maryam Naveed <maryaam2209@gmail.com>
2024-06-02 19:57:39 +0500

    fix: deployment issues

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,7 @@
-import data from "../data/data";
+import productData from "../data/data";
 
 export default function handler(req, res) {
-  const { Products } = data;
+  const { Products } = productData;
   if (Products) return res.status(200).json(Products);
   return res.status(200).json({ error: "Not found" });
 }
